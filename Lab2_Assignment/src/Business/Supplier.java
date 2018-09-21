2138bbd5c92dc31684ece93aa7f737fe4a3c531f
Urja Jain <jain.u@husky.neu.edu>
2018-09-20 23:04:27 -0400

    Lab 2 First Commit

---
+++ Lab2_Assignment/src/Business/Supplier.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author hp
+ */
+public class Supplier {
+    private String SupplierName;
+    private String SupplierAddress;
+
+    public String getSupplierName() {
+        return SupplierName;
+    }
+
+    public void setSupplierName(String SupplierName) {
+        this.SupplierName = SupplierName;
+    }
+
+    public String getSupplierAddress() {
+        return SupplierAddress;
+    }
+
+    public void setSupplierAddress(String SupplierAddress) {
+        this.SupplierAddress = SupplierAddress;
+    }
+    
+    public Supplier(){
+        SupplierName = "URJA JAIN";
+        SupplierAddress = "Boston Massachusetts" ;       
+        
+    }
+    
+}
